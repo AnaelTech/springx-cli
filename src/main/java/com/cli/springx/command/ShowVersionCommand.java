f153fa9e345c4b2dcf429dde9f9c36f263453308
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2025-07-03 20:11:35 +0200

    feat: ✨ Add version for cmd --version
    - TODO : Dynamic version

--- src/main/java/com/cli/springx/command/ShowVersionCommand.java
+++ src/main/java/com/cli/springx/command/ShowVersionCommand.java
@@ -1,13 +1,29 @@
 package com.cli.springx.command;
 
+import com.cli.springx.service.InputOutput;
+
 import picocli.CommandLine.Command;
 
 @Command(name = "--version", description = "Shows the version of the CLI")
 public class ShowVersionCommand implements Runnable {
 
+  private InputOutput inputOutput;
+
+  public ShowVersionCommand() {
+
+  }
+
+  public ShowVersionCommand(InputOutput inputOutput) {
+    this.inputOutput = inputOutput;
+  }
+
   @Override
   public void run() {
+    ShowVersion();
+  }
 
+  public void ShowVersion() {
+    inputOutput.print("SpringX CLI version 1.0.0");
   }
 
 }
